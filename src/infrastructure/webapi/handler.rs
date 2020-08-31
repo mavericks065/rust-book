bfc7118de995802b7611446848de89030f233e53
Thibaut Gery <thibaut.gery@gmail.com>
2020-08-31 12:10:11 +0200

    Add connection pool

--- src/infrastructure/webapi/handler.rs
+++ src/infrastructure/webapi/handler.rs
@@ -1,19 +1,22 @@
-
-use warp::{http::StatusCode, reject, reply::json, Reply, Rejection};
-use tokio::macros::support::Future;
+use crate::infrastructure::webapi::dtos::{CompanyDto, NewCompany};
+use diesel::r2d2::{ConnectionManager, PooledConnection};
 use diesel::PgConnection;
-use crate::infrastructure::webapi::dtos::{NewCompany, CompanyDto};
+use tokio::macros::support::Future;
+use warp::{http::StatusCode, reject, reply::json, Rejection, Reply};
 
 pub fn health_handler() -> String {
-    let result =  "health check: all good";
+    let result = "health check: all good";
     result.to_string()
 }
 
-pub fn create_new_company(new_company: NewCompany, connection: PgConnection) -> CompanyDto {
-    CompanyDto{
-        name : String::from(""),
-        description : String::from(" "),
+pub fn create_new_company(
+    new_company: NewCompany,
+    connection: &PooledConnection<ConnectionManager<PgConnection>>,
+) -> CompanyDto {
+    CompanyDto {
+        name: String::from(""),
+        description: String::from(" "),
         full_address: String::from(""),
-        abn : String::from("")
+        abn: String::from(""),
     }
 }
